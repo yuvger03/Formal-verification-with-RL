7f70edc85001ad2e0bb5b22d49bda3f613ba0e2b
yuvger03 <yuvger03@gmail.com>
2023-01-18 16:52:11 +0200

    new experiment

--- Frozen_lake/dockerScript.py
+++ Frozen_lake/dockerScript.py
@@ -2,7 +2,7 @@ import os
 
 for i in range(10):
     for j in range(3):
-        os.system(f"docker create --name c{i*10+j*3} --rm -v $HOME:$HOME -v /opt/gurobi:/opt/gurobi -w $HOME/softmax ubuntu_python3.8:1 python3 main.py {0.99 - 0.01*j} {0.14 +0.01*i} {i*10+j*3}")  # This is the command that opens the docker container
+        os.system(f"docker create --name c{i*10+j*3} --rm -v $HOME:$HOME -v /opt/gurobi:/opt/gurobi -w $HOME/softmax ubuntu_python3.8:1 python3 main.py {0.99 - 0.01*j} {1-i*0.01} {i*10+j*3}")  # This is the command that opens the docker container
         # os.system("python3 main.py")  # This is the command that runs the main.py file
         # os.system("cd ..")  # This is the command that goes back to the parent directory
         os.system(f"docker start c{i*10+j*3}")  # This is the command that starts the docker container
