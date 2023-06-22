5e463c54325453dab8bc3d34a040fda2afd57444
Liri-Be <liri.benzinou@gmail.com>
2023-06-22 11:50:10 +0300

    plot results

--- Catch_the_cheese/csvsToPLT.py
+++ Catch_the_cheese/csvsToPLT.py
@@ -3,12 +3,12 @@ import os
 import matplotlib.pyplot as plt
 
 
-def writeData(folder):
+def writeData(folder, res_file):
     for i in [1000]:
         entries = os.listdir(folder)
         os.chdir(folder)
 
-        resfile = open(f"resCTCNewFolder.csv", 'a', newline='')
+        resfile = open(res_fil, 'a', newline='')
         writer = csv.writer(resfile)
         fileStart = fr"{folder}/nuxmv_prism_results_"
         # for size in [10]:
@@ -18,6 +18,7 @@ def writeData(folder):
         for size in [10, 15, 20, 25, 30, 40]:
             RW(size, entries, writer)
         resfile.close()
+    # os.chdir("..")
 
 
 def RW(size, entries, writer):
@@ -34,7 +35,7 @@ def RW(size, entries, writer):
 
 
 def csvPlot(filename):
-    probs = [float(p / 100) for p in range(0, 100, 5)]
+    probs = [float(p / 100) for p in range(0, 105, 5)]
     data_size = {10: [], 15: [], 20: [], 25: [], 30: [], 40: []}
     with open(filename, 'r', newline='') as csvfile:
         currentSize = 10
@@ -48,20 +49,18 @@ def csvPlot(filename):
                 data_size[currentSize].append(float(row[2]))
 
     for s in [10, 15, 20, 25, 30, 40]:
-        if len(data_size[s]) == 20:
-            plt.plot(probs, data_size[s], '-o', label=str(s))
-        else:
-            plt.plot(probs + [1.00], data_size[s], '-o', label=str(s))
+        plt.plot(probs, data_size[s], '-o', label=str(s))
     plt.legend(title="Size")
     plt.xlabel("p")
     plt.ylabel("Probability of losing")
-    plt.title(f"Probability of losing vs p using {filename[6:-4]} iterations")
+    plt.title(f"Probability of losing vs p")  #  using {filename[6:-4]} iterations
     plt.savefig(filename[:-4] + ".png")
     plt.show()
 
 
 # csvPlot("resCTCNewfolder.csv")
-# writeData("./results")
+file_name = "resCTCTest.csv"
+writeData("./results", file_name)
 # print(os.path.abspath("./resCTCNewFolder.csv"))
-csvPlot("./results/resCTCNewFolder.csv")
+csvPlot(f"./{file_name}")
 
